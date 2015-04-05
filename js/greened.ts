8c5bc9400ed9037f299120320e518116f3d956be
Wojciech Andreas Jurczyk <wajurczyk@gmail.com>
2015-04-05 22:46:44 +0000

    added snapping to nodes and node highlighting, but found bug in zoom value usage

--- js/greened.ts
+++ js/greened.ts
@@ -15,12 +15,12 @@ module GreenEd {
 	
 	export class Editor {
 		
-		
-		
 		private currentMode:MODE = MODE.NONE;
 		
 		private colorBG:string = "rgb(0,0,0)";
 		private ZOOMSTEP:number = 0.25;
+		private SNAP_DISTANCE:number = 15;
+		private NODE_RADIUS:number = this.SNAP_DISTANCE;
 		
 		private canvas:HTMLCanvasElement = null;
 		private ctx:CanvasRenderingContext2D = null;
@@ -54,11 +54,12 @@ module GreenEd {
 				return false;
 			}
 			
+			this.currentMouseScreenPos = new Point(0,0);
+			this.lastMouseScreenPos = new Point(0,0);
+			
 			this.ctx.font = "48px serif";
 			
 			this.resizeCanvas();
-			this.currentMouseScreenPos = new Point(0,0);
-			this.lastMouseScreenPos = new Point(0,0);
 			
 			this.canvas.onmousedown = this.onMouseDown;
 			this.canvas.onmousemove = this.onMouseMove;
@@ -106,12 +107,23 @@ module GreenEd {
 				var p2OnScreen:Point = this.levelPosToScreenPos(wall.p2);
 				
 				this.ctx.beginPath();
-				this.ctx.arc(p1OnScreen.x, p1OnScreen.y, 10, 0, Math.PI*2, true); 
+				this.ctx.arc(p1OnScreen.x, p1OnScreen.y, this.NODE_RADIUS, 0, Math.PI*2, true); 
 				this.ctx.stroke();
 				
 				this.ctx.beginPath();
-				this.ctx.arc(p2OnScreen.x, p2OnScreen.y, 10, 0, Math.PI*2, true); 
+				this.ctx.arc(p2OnScreen.x, p2OnScreen.y, this.NODE_RADIUS, 0, Math.PI*2, true); 
 				this.ctx.stroke();
+			}
+			
+			/* highlight node under mouse */
+			var levelPos:Point = this.screenPosToLevelPos(this.currentMouseScreenPos);
+			var snappedPos:Point = this.snapToNextNode(levelPos);
+			if( null != snappedPos ) {
+				var snappedScreenPos:Point = this.levelPosToScreenPos(snappedPos);
+				this.ctx.fillStyle = 'rgb(0,255,0)';
+				this.ctx.beginPath();
+				this.ctx.arc(snappedScreenPos.x, snappedScreenPos.y, this.NODE_RADIUS, 0, Math.PI*2, true); 
+				this.ctx.fill();
 			}
 		}
 		
@@ -150,6 +162,10 @@ module GreenEd {
 		
 		private onMouseDown = (evt:MouseEvent) => {
 			this.mouseDownLevelPos = this.screenPosToLevelPos(this.getScreenMousePos(evt));
+			var snappedPos:Point = this.snapToNextNode(this.mouseDownLevelPos);
+			if( null != snappedPos ) {
+				this.mouseDownLevelPos = snappedPos;
+			}
 			this.redraw();
 		}
 		
@@ -161,15 +177,28 @@ module GreenEd {
 					this.currentOffset.x -= diff.x / this.currentZoom;
 					this.currentOffset.y -= diff.y / this.currentZoom;
 				}
-				
-				this.redraw();
+				else if( this.currentMode == MODE.WALLS ) {
+					var levelPos:Point = this.screenPosToLevelPos(this.currentMouseScreenPos);
+					var snappedPos:Point = this.snapToNextNode(levelPos);
+					if( null != snappedPos ) {
+						levelPos = snappedPos;
+					}
+					this.currentMouseScreenPos = this.levelPosToScreenPos(levelPos);
+				}
 			}
 			this.lastMouseScreenPos = this.currentMouseScreenPos;
+				
+			this.redraw();
 		}
 		
 		private onMouseUp = (evt:MouseEvent) => {
 			if( this.currentMode == MODE.WALLS && null != this.mouseDownLevelPos ) {
-				var levelPosMouseUp:Point = this.screenPosToLevelPos(this.getScreenMousePos(evt));
+				var screenPos:Point = this.getScreenMousePos(evt);
+				var levelPosMouseUp:Point = this.screenPosToLevelPos(screenPos);
+				var snappedPos:Point = this.snapToNextNode(levelPosMouseUp);
+				if( null != snappedPos ) {
+					levelPosMouseUp = snappedPos;
+				}
 				var newWall:Wall = new Wall(this.mouseDownLevelPos, levelPosMouseUp);
 				this.walls.push(newWall);
 			}
@@ -218,6 +247,30 @@ module GreenEd {
 			screenPos.x = screenPos.x * this.currentZoom;
 			screenPos.y = screenPos.y * this.currentZoom;
 			return screenPos;
+		}
+		
+		/**
+		 * takes a levelPos, looks for snapable nodes and returns the snapped to levelPos
+		 * if no node with SNAP_DISTANCE, returns null
+		 */
+		private snapToNextNode(levelPos:Point) : Point {
+			for( var i:number = 0; i < this.walls.length; ++i ) {
+				var wall:Wall = this.walls[i];
+				var distance:number = this.getDistance(wall.p1, levelPos);
+				if( distance <= this.SNAP_DISTANCE ) {
+					return wall.p1;
+				}
+				distance = this.getDistance(wall.p2, levelPos);
+				if( distance <= this.SNAP_DISTANCE ) {
+					return wall.p2;
+				}
+			}
+			return null;
+		}
+		
+		private getDistance(p1:Point, p2:Point) : number {
+			var vec:Point = new Point(p1.x - p2.x, p1.y - p2.y);
+			return Math.sqrt(vec.x * vec.x + vec.y * vec.y);
 		}
 	}
 }
